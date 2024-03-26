8678366ac31d2215e1b8afc9f1b991c7919d5d29
ihzavip <ihzavip@gmail.com>
2024-03-26 21:07:10 +0700

    laptop 260324

--- src/app/courses/course-detail/course-detail.component.ts
+++ src/app/courses/course-detail/course-detail.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CourseService } from 'src/app/Services/course.service';
 
 @Component({
   selector: 'app-course-detail',
   templateUrl: './course-detail.component.html',
   styleUrls: ['./course-detail.component.css']
 })
-export class CourseDetailComponent{
+export class CourseDetailComponent implements OnInit {
+
+  _courseService: CourseService = inject(CourseService);
+  _activeRoute: ActivatedRoute = inject(ActivatedRoute);
+
+  courseId: any;
+
+  ngOnInit() {
+    this.courseId = this._activeRoute.snapshot.paramMap.get('id');
+    console.log(this._activeRoute.snapshot.paramMap.get('id'));
+
+  }
 
 }
